3fbebff0925743674018f817a5e915e034987334
Vamsi <vamsivicky1998@gmail.com>
2026-06-23 13:43:30 +0000

    Day 10 - Package Management SSH and Python Basics

---
+++ day10-python/loop.py
@@ -0,0 +1,5 @@
+for i in range(1,20):
+    print(i)
+
+for i in range(a,k):
+    print(i)
---
+++ day10-python/python.py
@@ -0,0 +1,21 @@
+print ("hi vamsi ")
+
+name="krinshna"
+print(name)
+
+names=input("enter your name :")
+print("hello",names)
+
+age = 15
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+
+age = int(input("enter your age : "))
+if age >= 18:
+    print("eligebile to vote")
+else:
+    print("not eligebile to vote" )
